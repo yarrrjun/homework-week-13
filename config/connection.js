6969a6951b176bd0ef3d0408d9916fccd4de57b2
Kornajun Roy <yarrrjun@gmail.com>
2020-08-02 16:36:05 -0400

    modified connection.js to include a JawsDB URL

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "h0nd@c1vIC",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "h0nd@c1vIC",
+    database: "burgers_db"
+  });
+
+}
+
+
 
 
 connection.connect(function(err) {
