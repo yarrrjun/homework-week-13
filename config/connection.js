8749a292f162e4564bbabd8f1d5d30dd4eff4a36
Kornajun Roy <yarrrjun@gmail.com>
2020-08-02 17:09:37 -0400

    attempted to add JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,27 @@
 var mysql = require("mysql");
-var connection;
+// var connection;
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "h0nd@c1vIC",
-    database: "burgers_db"
-  });
+// if (process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "h0nd@c1vIC",
+//     database: "burgers_db"
+//   });
 
-}
+// }
+// couldn't get that working; MySQL Workbench remote connection to JawsDB was too confusing; will just have to store the details here for now until I can figure out how to do this
 
+var connection = mysql.createConnection({
+  host: "s554ongw9quh1xjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "enpuy205fb3ezmes",
+  password: "l2ybj1x7verisj2c",
+  database: "caqixvjgphi617ql"
+});
 
 
 
